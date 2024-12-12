ebee7e5e87bbf42f491ade82dee6cefa2027ac4a
VuongDuc12 <vuongduc226@gmail.com>
2024-12-12 15:11:55 +0700

    Nộp TH_Bai3

---
+++ database/seeders/IssuesTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
+
+class IssuesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $faker = Faker::create();
+
+        foreach (range(1, 30) as $index) {
+            DB::table('issues')->insert([
+                'computer_id' => $faker->numberBetween(1, 20), // Giả định có 20 máy tính
+                'reported_by' => $faker->name,
+                'reported_date' => $faker->dateTimeBetween('-1 month', 'now'),
+                'description' => $faker->sentence(10),
+                'urgency' => $faker->randomElement(['Low', 'Medium', 'High']),
+                'status' => $faker->randomElement(['Open', 'In Progress', 'Resolved']),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
+    }
+}
